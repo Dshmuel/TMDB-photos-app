51c4ffe43ec3eafc970c9b948906d9563f759428
Shmuel Dimov <shmuel@action-item.co.il>
2023-12-06 14:14:38 +0200

    Fixed bug: server data can be null

--- app/src/main/java/com/dimovsoft/shutterfly/model/Genre.kt
+++ app/src/main/java/com/dimovsoft/shutterfly/model/Genre.kt
@@ -1,1 +1,1 @@
-package com.dimovsoft.shutterfly.modelimport com.google.gson.annotations.SerializedNamedata class Genre(	@SerializedName("id") val id: Int,	@SerializedName("name") val name: String,)data class GenresResponse(	@SerializedName("genres") val genres: List<Genre>,)
+package com.dimovsoft.shutterfly.modelimport com.google.gson.annotations.SerializedNamedata class Genre(	@SerializedName("id") val id: Int?,	@SerializedName("name") val name: String?,)data class GenresResponse(	@SerializedName("genres") val genres: List<Genre>?,)
